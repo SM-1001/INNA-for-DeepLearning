441f2d36621cd1856f79bbc1fcdfa0771a845256
SM-1001 <53719845+SM-1001@users.noreply.github.com>
2021-04-30 18:18:34 +0900

    Update inna.py

--- INNA_for_tensorflow/inna.py
+++ INNA_for_tensorflow/inna.py
@@ -215,9 +215,9 @@ class NADIANOptimizer(optimizer.Optimizer):
     v_temp = cond( equal(num_iter(),0) ,
       lambda : (1.-alpha_t*beta_t) * var - beta_t**2 * grad + beta_t * speed_ini_t * grad, lambda : v )
          
-    g = self.get_slot(var, "g1")
+    g = self.get_slot(grad, "g1")
     g_temp = cond( equal(num_iter(),0) ,
-        lambda : (grad, lambda : g )
+        lambda : grad, lambda : g )
     
     v_t = v.assign( v_temp - ( lr_t * decay_t / math_ops.pow(math_ops.cast(num_iter()+1, var.dtype.base_dtype),decaypower_t) ) * ( (alpha_t-1./beta_t) * var + 1./beta_t * v_temp ) )
    
