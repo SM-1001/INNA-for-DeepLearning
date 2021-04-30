516bdb725cc493001f52c690bcb7a3c77928d301
SM-1001 <53719845+SM-1001@users.noreply.github.com>
2021-04-30 18:35:09 +0900

    Update inna.py

--- INNA_for_tensorflow/inna.py
+++ INNA_for_tensorflow/inna.py
@@ -223,7 +223,7 @@ class NADIANOptimizer(optimizer.Optimizer):
    
     var_update = state_ops.assign_sub( var, ( lr_t * decay_t / math_ops.pow(math_ops.cast(num_iter()+1, var.dtype.base_dtype),decaypower_t) ) * ( (alpha_t-1./beta_t) * var + 1./beta_t * v_temp + beta_t * ((1. + mu_t) * grad - mu_t * pre_g) ) #Update 'ref' by subtracting 'value
                               
-    pre_g = g.assign(g_temp)
+    pre_g = g.assign( g_temp )
                                       
     return control_flow_ops.group(*[var_update, v_t])
     
