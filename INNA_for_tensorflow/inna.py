90724baffd4f85c25b1e029a9715af18fb1a8870
SM-1001 <53719845+SM-1001@users.noreply.github.com>
2021-05-01 19:35:01 +0900

    Update inna.py

--- INNA_for_tensorflow/inna.py
+++ INNA_for_tensorflow/inna.py
@@ -27,6 +27,7 @@ Args:
     speed_ini: The initial velocity is in the direction of the speepest descent:
     it is in exactly - speed_ini* gradient. Default is 1., we recommend to keep this parameter unchanged.
 """
+import tensorflow as tf
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
