0e05a93d1f98beac3362d2d13306aadb4186991c
SM-1001 <53719845+SM-1001@users.noreply.github.com>
2021-05-09 12:15:29 +0900

    Update nadi.py

--- INNA_for_keras/nadi.py
+++ INNA_for_keras/nadi.py
@@ -92,9 +92,9 @@ class NADIAN(Optimizer):
             #p_t = p + lr_t * ( (1./self.beta - self.alpha) * p - 1./self.beta * v_temp - self.beta * ((1. + self.mu) * g - self.mu * pre_g))
             p_t = p + lr_t * ( (1./self.beta - self.alpha) * p - 1./self.beta * v_temp - self.beta * g)
              
-            new_p = K.switch(K.equal( self.iterations , 0 ),
-                        p_t , p_t + self.mu * (p_t - p))    
-            #new_p = p_t + self.mu * (p_t - p)
+            #new_p = K.switch(K.equal( self.iterations , 0 ),
+             #           p_t , p_t + self.mu * (p_t - p))    
+            new_p = p_t + self.mu * (p_t - p)
             # Apply constraints.
             if getattr(p, 'constraint', None) is not None:
                 new_p = p.constraint(new_p)
