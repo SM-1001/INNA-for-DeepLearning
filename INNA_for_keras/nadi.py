8ea9a48a394b15a226dc29704736f642dfae8b22
SM-1001 <53719845+SM-1001@users.noreply.github.com>
2021-05-09 10:50:44 +0900

    Update nadi.py

--- INNA_for_keras/nadi.py
+++ INNA_for_keras/nadi.py
@@ -61,8 +61,8 @@ class NADIAN(Optimizer):
     @interfaces.legacy_get_updates_support
     def get_updates(self, loss, params):
         global c, pre_params
-        if self.iterations == 0:
-            pre_params = params
+        #if self.iterations == 0:
+        pre_params = params
        #grads = self.get_gradients(loss, params)
         c = K.switch(self.iterations == 0, params, params + self.mu *(params - pre_params))
         grads = self.get_gradients(loss, c)
