c3a8fb8f877af4fd0a98318de3cfabc934780d95
SM-1001 <53719845+SM-1001@users.noreply.github.com>
2021-05-10 17:08:52 +0900

    Update nadi2.py

--- INNA_for_keras/nadi2.py
+++ INNA_for_keras/nadi2.py
@@ -94,7 +94,7 @@ class NADIAN(Optimizer):
             p_t = p + lr_t * ( (1./self.beta - self.alpha) * p - 1./self.beta * v_temp - self.beta * g)
             #p_t = p - lr_t * ( (self.alpha - 1./self.beta) * p + 1./self.beta * v_temp + self.beta * g)
              
-            print("p_t=", p_t.numpy())
+            tf.print("p_t=", p_t)
             #df = pd.DataFrame(p_t)
             #df.to_csv('p_t.csv')
             #p_t = (-1.* p_t) + self.mu*((-1. * p_t) - p)
